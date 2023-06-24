3b4ece734a760a1d8cefcb155d6dd58144b4e3b6
juanferjaramillo <juanfer.jaramillo@gmail.com>
2023-06-23 22:38:24 -0500

    flip card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -10,7 +10,7 @@ import {
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import Badge from "@mui/material/Badge";
-import style from "./card.module.css";
+import ReactCardFlip from "react-card-flip";
 
 let colorD = "white";
 
@@ -68,6 +68,9 @@ const colorsDot = (id) => {
   }
 };
 
+const handleClick = () => {
+  setFlipped((flip) => !flip);
+};
 //-------------------------COMPONENT------------------------
 function Card(props) {
   const [flipped, setFlipped] = useState(false);
@@ -83,151 +86,157 @@ function Card(props) {
     setFlipped(!flipped);
   };
 
-  colorsDot(props.estado)
+  colorsDot(props.estado);
 
-  return (
+  //-------------------------------BACK CARD-----------------------------------
+  const backCard = (
     <Box
       key={props.ind}
       margin={1}
-      // border={1}
-      className={flipped ? style.backCard : style.frontCard}
+      // onMouseLeave={handleClick}
+      onClick={handleClick}
       sx={{
-        // width: isMobile ? "80vw" : "25vw",
-        // minWidth: "320px",
         width: "320px",
-        // minWidth: isMobile ? "310px" : "310px",
+        height: "530px",
         bgcolor: theme.palette.background.paper,
         boxShadow: 8,
         borderRadius: 2,
         p: 1,
-        // borderColor: "purple",
-        // borderColor: "lightgray",
       }}
     >
-      {flipped ? (
-        //-------------------------------BACK CARD-----------------------------------
+      <Grid
+        container
+        direction={"column"}
+        alignItems={"center"}
+        justifyContent={"space-around"}
+        sx={{
+          height: "500px",
+          cursor: "pointer",
+        }}
+      >
+        <Typography variant="body1">{`Codigo: ${props.Barras}`}</Typography>
 
-        <Grid
-          container
-          direction={"column"}
-          alignItems={"center"}
-          justifyContent={"space-around"}
-          onMouseLeave={handleClick}
-          onClick={handleClick}
-          sx={{
-            height: "500px",
-            className: "style.turn",
-            cursor: "pointer",
-          }}
-        >
-          <Typography variant="body1">{`Codigo: ${props.Barras}`}</Typography>
+        {/* EXISTENCIA EN INVENTARIO------------------------------------------------------ */}
+        <Typography variant="body2">{`0315`}</Typography>
 
-          {/* EXISTENCIA EN INVENTARIO------------------------------------------------------ */}
-          <Typography variant="body2">{`0315`}</Typography>
+        <Typography fontSize={13} textAlign="justify" p={1} boxShadow={2}>
+          {props.descripcion}
+        </Typography>
 
-          <Typography fontSize={13} textAlign="justify" p={1} boxShadow={2}>
-            {props.descripcion}
-          </Typography>
+        <img
+          style={{ objectFit: "contain" }}
+          src={props.prodImg}
+          height="150vh"
+          width="240vh"
+          alt="producto"
+          // border="1"
+        />
 
-          <img
-            style={{ objectFit: "contain" }}
-            src={props.prodImg}
-            height="150vh"
-            width="240vh"
-            alt="producto"
-            // border="1"
-          />
-
-          <Divider sx={{ width: "80%" }} />
-          <Grid item display={"flex"} justifyContent={"center"} marginTop={1}>
-            {props.icons?.map((icon, i) => {
-              return (
-                <Avatar
-                  key={i}
-                  alt="icon"
-                  src={icon.iconUrl}
-                  sx={{
-                    width: 40,
-                    height:40,
-                    marginRight: 0.5,
-                    marginLeft: 0.5,
-                  }}
-                ></Avatar>
-              );
-            })}
-          </Grid>
-        </Grid>
-      ) : (
-        //-------------------------------FRONT CARD-----------------------------------
-        <Grid
-          container
-          direction={"column"}
-          alignItems={"center"}
-          justifyContent={"space-around"}
-          onClick={handleClick}
-          sx={{
-            height: "500px",
-            cursor: "pointer",
-          }}
-
-        >
-          <StyledBadge
-            overlap="circular"
-            anchorOrigin={{ vertical: "top", horizontal: "right" }}
-            badgeContent=""
-            variant="dot"
-          >
-  
-              <Typography
-                variant="body1"
+        <Divider sx={{ width: "80%" }} />
+        <Grid item display={"flex"} justifyContent={"center"} marginTop={1}>
+          {props.icons?.map((icon, i) => {
+            return (
+              <Avatar
+                key={i}
+                alt="icon"
+                src={icon.iconUrl}
                 sx={{
-                  width: "310px",
-                  textAlign: "center",
+                  width: 40,
+                  height: 40,
+                  marginRight: 0.5,
+                  marginLeft: 0.5,
                 }}
-              >
-                {`Código: ${props.id}`}
-              </Typography>
-       
-          </StyledBadge>
+              ></Avatar>
+            );
+          })}
+        </Grid>
+      </Grid>
+    </Box>
+  );
 
-        
-
-          <img
-            style={{ objectFit: "contain" }}
-            src={props.prodImg}
-            height="220vh"
-            width="300vh"
-            alt="producto"
-            // border="1"
-          />
-
+  //-------------------------------FRONT CARD-----------------------------------
+  const frontCard = (
+    <Box
+      key={props.ind}
+      margin={1}
+      onClick={handleClick}
+      sx={{
+        width: "320px",
+        height: "530px",
+        bgcolor: theme.palette.background.paper,
+        boxShadow: 8,
+        borderRadius: 2,
+        p: 1,
+      }}
+    >
+      <Grid
+        container
+        direction={"column"}
+        alignItems={"center"}
+        justifyContent={"space-around"}
+        sx={{
+          height: "500px",
+          cursor: "pointer",
+        }}
+      >
+        <StyledBadge
+          overlap="circular"
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          badgeContent=""
+          variant="dot"
+        >
           <Typography
-            variant="h6"
-            fontWeight={"bold"}
+            variant="body1"
             sx={{
-              textAlign: "center",
               width: "310px",
-              p: 1,
+              textAlign: "center",
             }}
           >
-            {props.nombre}
+            {`Código: ${props.id}`}
           </Typography>
-          <Divider sx={{ width: "80%" }} />
-          <Typography variant="body2">{`Precio sin IVA: $ ${PB}`}</Typography>
-          <Typography variant="body2">{`Precio con IVA: $ ${PT}`}</Typography>
+        </StyledBadge>
 
-          <Grid item width={300} textAlign={"center"}>
-         
-            <Typography variant="body2">
-              {props.categoria}
-              </Typography>
-            {/* ); */}
-            {/* })} */}
-          </Grid>
+        <img
+          style={{ objectFit: "contain" }}
+          src={props.prodImg}
+          height="220vh"
+          width="300vh"
+          alt="producto"
+          // border="1"
+        />
 
+        <Typography
+          variant="h6"
+          fontWeight={"bold"}
+          sx={{
+            textAlign: "center",
+            width: "310px",
+            p: 1,
+          }}
+        >
+          {props.nombre}
+        </Typography>
+        <Divider sx={{ width: "80%" }} />
+        <Typography variant="body2">{`Precio sin IVA: $ ${PB}`}</Typography>
+        <Typography variant="body2">{`Precio con IVA: $ ${PT}`}</Typography>
+
+        <Grid item width={300} textAlign={"center"}>
+          <Typography variant="body2">{props.categoria}</Typography>
+          {/* ); */}
+          {/* })} */}
         </Grid>
-      )}
+      </Grid>
     </Box>
+  );
+
+  return (
+    <ReactCardFlip isFlipped={flipped} flipDirection="horizontal"
+    flipSpeedBackToFront={0.8}
+    flipSpeedFrontToBack={0.8}
+    >
+      {frontCard}
+      {backCard}
+    </ReactCardFlip>
   );
 }
 
